4b6a11a73e8e78c958b9c2a6b43960bf14852362
MrLogic <sulleyabdullatif404@gmail.com>
2025-12-31 09:05:54 +0000

    feat: Add `fetchMerchant` prop to MerchantOnboarding component.

--- src/components/merchant/MerchantOnboarding.tsx
+++ src/components/merchant/MerchantOnboarding.tsx
@@ -19,6 +19,7 @@ export function MerchantOnboarding() {
     banks, 
     loadingBanks, 
     fetchBanks, 
+    fetchMerchant,
     verifyBankAccount, 
     createMerchant, 
     setupPaystackSubaccount 
